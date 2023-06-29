0c44cc3a7ca2ec032b5c2e5258af2be0665a5cc4
Srijan Deo <88278620+SrijanDeo-DA-DS@users.noreply.github.com>
2023-06-28 21:03:32 -0500

    updates

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,36 +1,101 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
 import seaborn as sns
-import matplotlib.pyplot as plt
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+# Modelling
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge,Lasso
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+import warnings
 
 df_train = pd.read_csv('C:/Users/srija/Documents/DS_Job_Salary_Predictor/data/data_preprocess.csv')
 
 from sklearn.model_selection import train_test_split
 
-X = df_train[['Easy_Apply','python_yn','R_yn','spark','aws','excel','age_of_company','num_comp','Job_Title_simp_data_engineer','Job_Title_simp_data_scientist','Job_Seniority_jr','Job_Seniority_manager','Job_Seniority_na','Job_Seniority_senior','job_state_MA','job_state_NY','job_headquarters_MA','job_headquarters_NY']]
+X = df_train.drop(['avg_salary'],axis=1)
 y = df_train['avg_salary']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-rf = RandomForestRegressor()
+def evaluate_model(true, predicted):
+    mae = mean_absolute_error(true, predicted)
+    mse = mean_squared_error(true, predicted)
+    rmse = np.sqrt(mean_squared_error(true, predicted))
+    r2_square = r2_score(true, predicted)
+    return mae, rmse, r2_square
 
-np.mean(cross_val_score(rf,X_train,y_train,scoring = 'neg_mean_absolute_error', cv= 3))
+models = {
+    "Linear Regression": LinearRegression(),
+    "Lasso": Lasso(),
+    "Ridge": Ridge(),
+    "K-Neighbors Regressor": KNeighborsRegressor(),
+    "Decision Tree": DecisionTreeRegressor(),
+    "Random Forest Regressor": RandomForestRegressor(),
+    "XGBRegressor": XGBRegressor(), 
+    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+    "AdaBoost Regressor": AdaBoostRegressor()
+}
+model_list = []
+r2_list =[]
 
-rf.fit(X_train,y_train)
+for i in range(len(list(models))):
+    model = list(models.values())[i]
+    model.fit(X_train, y_train) # Train model
 
-y_pred = rf.predict(X_test)
+    # Make predictions
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    
+    # Evaluate Train and Test dataset
+    model_train_mae , model_train_rmse, model_train_r2 = evaluate_model(y_train, y_train_pred)
 
-from sklearn.metrics import mean_absolute_error
-mean_absolute_error(y_test,y_pred)
+    model_test_mae , model_test_rmse, model_test_r2 = evaluate_model(y_test, y_test_pred)
+
+    
+    print(list(models.keys())[i])
+    model_list.append(list(models.keys())[i])
+    
+    print('Model performance for Training set')
+    print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+    print("- R2 Score: {:.4f}".format(model_train_r2))
+
+    print('----------------------------------')
+    
+    print('Model performance for Test set')
+    print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+    print("- R2 Score: {:.4f}".format(model_test_r2))
+    r2_list.append(model_test_r2)
+    
+    print('='*35)
+    print('\n')
+
+abc = pd.DataFrame(list(zip(model_list, r2_list)), columns=['Model Name', 'R2_Score']).sort_values(by=["R2_Score"],ascending=False)
+pred_df=pd.DataFrame({'Actual Value':y_test,'Predicted Value':y_test_pred,'Difference':y_test-y_test_pred})
+
+
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,300,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
+
+gs = GridSearchCV(RandomForestRegressor,parameters,scoring='neg_mean_absolute_error',cv=3)
+gs.fit(X_train,y_train)
+
+gs.best_score_
+gs.best_estimator_
+
 
 import pickle
 # open a file, where you ant to store the data
 file = open('C:/Users/srija/Documents/DS_Job_Salary_Predictor/data/random_forest_regression_model.pkl', 'wb')
 
 # dump information to that file
-pickle.dump(rf, file)
+pickle.dump(RandomForestRegressor, file)
